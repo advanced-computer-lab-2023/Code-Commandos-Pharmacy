080e22b3c135a08538d402c965e003e002c08390
Arwa <arwa_wail@hotmail.com>
2023-10-07 03:51:03 +0300

    backend push

--- backend/model/Medicine.js
+++ backend/model/Medicine.js
@@ -1,11 +1,18 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
+
 const MedicineSchema = new Schema({
         name: {
             type: String,
             unique: true,
-            required: true
+            required: true,
+            image: {
+                type: String,
+                default: function () {
+                    return '../images/' + this.type + '.png';
+                }
+            }
         },
         description: {
             type: String,
