8c619c37ec3fead21de3f8bcd7204ced42b8b885
Arwa <arwa_wail@hotmail.com>
2023-10-07 03:54:51 +0300

    view picture

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,33 @@
+import {useEffect, useState} from "react";
+
+
+//
+const Home = () => {
+    const [medicines, setMedicines] = useState(null)
+    useEffect(() => {
+        const fetchMedicines = async () => {
+            const response = await fetch('/api/medicine/viewAvailableMedicines')
+            // We get here an array of Medicine objects
+            const json = await response.json()
+            if (response.ok) {
+                setMedicines(json)
+            }
+        }
+        fetchMedicines()
+    }, [])
+
+    return (
+        <div className="available-medicines">
+            {medicines && medicines.map((medicine) => (
+                <div key={medicine._id}>
+                    <img src={medicine.image} alt={medicine.name} />
+                    <p>{medicine.name}</p>
+                    <p>{medicine.price}</p>
+                    <p>{medicine.description}</p>
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default Home
