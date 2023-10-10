c7918ad3f2b918f93f9b853a9b901ef4f00ac276
ebramfadl <ebram@ntsal.com>
2023-10-11 02:50:54 +0300

    wipe out pharmacy and all first 8 requirements done

--- frontend/src/components/PatientRegistrationForm.js
+++ frontend/src/components/PatientRegistrationForm.js
@@ -19,144 +19,190 @@ const PatientRegistrationForm = () => {
     e.preventDefault()
 
     const patient = {username: username, name: name, email: email, password: password, dateOfBirth: dateOfBirth, gender: gender, mobileNumber: mobileNumber, emergencyContact: {fullName: ecFullName, mobileNumber: ecMobileNumber, relationToPatient: ecRelation}}
-    
-    const response = await fetch('/api/pharmacyPatient/createPatient', {
-      method: 'POST',
-      body: JSON.stringify(patient),
-      headers: {
-        'Content-Type': 'application/json'
+
+    try {
+
+      const response = await fetch('/api/pharmacyPatient/createPatient', {
+        method: 'POST',
+        body: JSON.stringify(patient),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      const json = await response.json()
+
+      if (!response.ok) {
+        setError(json.message)
+        alert(await response.text())
       }
-    })
-    const json = await response.json()
-
-    if (!response.ok) {
-      setError(json.message)
+      if (response.ok) {
+        setError(null)
+        setName('')
+        setUsername('')
+        setEmail('')
+        setPassword('')
+        setGender('')
+        setMobileNumber('')
+        setEcFullName('')
+        setEcMobileNumber('')
+        setEcRelation('')
+        setNewPatient(json)
+        console.log('new patient registered:', json)
+      }
     }
-    if (response.ok) {
-      setError(null)
-      setName('')
-      setUsername('')
-      setEmail('')
-      setPassword('')
-      setGender('')
-      setMobileNumber('')
-      setEcFullName('')
-      setEcMobileNumber('')
-      setEcRelation('')
-      setNewPatient(json)
-      console.log('new patient registered:', json)
+    catch (error){
+      alert(error.message)
     }
-
   }
 
   return (
-      <div>
-    <form className="create" onSubmit={handleSubmit}> 
-      <h2>Registration:</h2>
-
-      <label>Username:</label>
-      <input 
-        type="text" 
-        onChange={(e) => setUsername(e.target.value)} 
-        value={username}
-      /> <br />
-
-      <label>Name:</label>
-      <input 
-        type="text" 
-        onChange={(e) => setName(e.target.value)} 
-        value={name}
-      /> <br />
-
-      <label>E-mail:</label>
-      <input 
-        type="email" 
-        onChange={(e) => setEmail(e.target.value)} 
-        value={email} 
-      /> <br />
-
-      <label>Password:</label>
-      <input 
-        type="password" 
-        onChange={(e) => setPassword(e.target.value)} 
-        value={password} 
-      /> <br />
-
-      <label>Date of Birth:</label>
-      <input 
-        type="date" 
-        onChange={(e) => setDateOfBirth(e.target.value)} 
-        value={dateOfBirth} 
-      /> <br />
-
-      <label>Gender:</label><br />
-      <input 
-        type="radio"
-        id="MALE"
-        name="gender" 
-        onChange={(e) => setGender(e.target.id)} 
-        //value={} 
-      /> 
-      <label>Male</label><br />
-      <input 
-        type="radio"
-        id="FEMALE"
-        name="gender"  
-        onChange={(e) => setGender(e.target.id)} 
-        //value={false} 
-      /> 
-      <label>Female</label><br />
-
-      <label>Mobile Number:</label>
-      <input 
-        type="text" 
-        onChange={(e) => setMobileNumber(e.target.value)} 
-        value={mobileNumber} 
-      /> <br />
-
-      <label>Emergency Contact Full Name:</label>
-      <input 
-        type="text" 
-        onChange={(e) => setEcFullName(e.target.value)} 
-        value={ecFullName} 
-      /> <br />
-
-      <label>Emergency Contact Mobile Number:</label>
-      <input 
-        type="text" 
-        onChange={(e) => setEcMobileNumber(e.target.value)} 
-        value={ecMobileNumber} 
-      /> <br />
-
-      <label>Relation to Emergency Contact:</label><br />
-      <input 
-        type="radio"
-        id="HUSBAND"
-        name="relation" 
-        onChange={(e) => setEcRelation(e.target.id)}  
-      /> 
-      <label>Husband</label><br />
-      <input 
-        type="radio"
-        id="WIFE"
-        name="relation" 
-        onChange={(e) => setEcRelation(e.target.id)}  
-      /> 
-      <label>Wife</label><br />
-      <input 
-        type="radio"
-        id="CHILDREN"
-        name="relation" 
-        onChange={(e) => setEcRelation(e.target.id)}  
-      /> 
-      <label>Son/Daughter</label><br />
-
-      <button>Register</button>
-      {error && <div className="error">{error}</div>}
-    </form>
-    {newPatient && <PatientDetails patient={newPatient} />}
+      <div className="container mt-4">
+        <h1 className="mb-4">Registration:</h1>
+        <form className="create" onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label htmlFor="username" className="form-label">
+              Username:
+            </label>
+            <input
+                type="text"
+                className="form-control"
+                id="username"
+                onChange={(e) => setUsername(e.target.value)}
+                value={username}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="name" className="form-label">
+              Name:
+            </label>
+            <input
+                type="text"
+                className="form-control"
+                id="name"
+                onChange={(e) => setName(e.target.value)}
+                value={name}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">
+              E-mail:
+            </label>
+            <input
+                type="email"
+                className="form-control"
+                id="email"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password" className="form-label">
+              Password:
+            </label>
+            <input
+                type="password"
+                className="form-control"
+                id="password"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="dateOfBirth" className="form-label">
+              Date of Birth:
+            </label>
+            <input
+                type="date"
+                className="form-control"
+                id="dateOfBirth"
+                onChange={(e) => setDateOfBirth(e.target.value)}
+                value={dateOfBirth}
+            />
+          </div>
+          <div className="mb-3">
+            <label className="form-label">Gender:</label><br />
+            <input
+                type="radio"
+                id="MALE"
+                name="gender"
+                onChange={(e) => setGender(e.target.id)}
+            />
+            <label htmlFor="MALE">Male</label><br />
+            <input
+                type="radio"
+                id="FEMALE"
+                name="gender"
+                onChange={(e) => setGender(e.target.id)}
+            />
+            <label htmlFor="FEMALE">Female</label><br />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="mobileNumber" className="form-label">
+              Mobile Number:
+            </label>
+            <input
+                type="text"
+                className="form-control"
+                id="mobileNumber"
+                onChange={(e) => setMobileNumber(e.target.value)}
+                value={mobileNumber}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="ecFullName" className="form-label">
+              Emergency Contact Full Name:
+            </label>
+            <input
+                type="text"
+                className="form-control"
+                id="ecFullName"
+                onChange={(e) => setEcFullName(e.target.value)}
+                value={ecFullName}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="ecMobileNumber" className="form-label">
+              Emergency Contact Mobile Number:
+            </label>
+            <input
+                type="text"
+                className="form-control"
+                id="ecMobileNumber"
+                onChange={(e) => setEcMobileNumber(e.target.value)}
+                value={ecMobileNumber}
+            />
+          </div>
+          <div className="mb-3">
+            <label className="form-label">Relation to Emergency Contact:</label><br />
+            <input
+                type="radio"
+                id="HUSBAND"
+                name="relation"
+                onChange={(e) => setEcRelation(e.target.id)}
+            />
+            <label htmlFor="HUSBAND">Husband</label><br />
+            <input
+                type="radio"
+                id="WIFE"
+                name="relation"
+                onChange={(e) => setEcRelation(e.target.id)}
+            />
+            <label htmlFor="WIFE">Wife</label><br />
+            <input
+                type="radio"
+                id="CHILDREN"
+                name="relation"
+                onChange={(e) => setEcRelation(e.target.id)}
+            />
+            <label htmlFor="CHILDREN">Son/Daughter</label><br />
+          </div>
+          <button className="btn btn-primary">Register</button>
+          {error && <div className="error">{error}</div>}
+        </form>
+        {newPatient && <PatientDetails patient={newPatient} />}
       </div>
-  )
+  );
+
 }
 
 export default PatientRegistrationForm
--- frontend/src/components/PharmacistRegistrationForm.js
+++ frontend/src/components/PharmacistRegistrationForm.js
@@ -17,102 +17,148 @@ const PharmacistRegistrationForm = () => {
     e.preventDefault()
 
     const pharmacistRequest = {username: username, name: name, email: email, password: password, dateOfBirth: dateOfBirth, hourlyRate: hourlyRate, affiliation: affiliation, educationalBackground: educationalBackground}
-    
-    const response = await fetch('/api/pharmacistRequest/addPharmacistRequest', {
-      method: 'POST',
-      body: JSON.stringify(pharmacistRequest),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-    const json = await response.json()
 
-    if (!response.ok) {
-      setError(json.message)
-    }
-    if (response.ok) {
-      setError(null)
-      setName('')
-      setUsername('')
-      setEmail('')
-      setPassword('')
-      setHourlyRate('')
-      setAffiliation('')
-      setEducationalBackground('')
+    try {
+
+      const response = await fetch('/api/pharmacistRequest/addPharmacistRequest', {
+        method: 'POST',
+        body: JSON.stringify(pharmacistRequest),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      const json = await response.json()
+
+      if (!response.ok) {
+        setError(json.message)
+        alert(await response.text())
+      }
+      if (response.ok) {
+        setError(null)
+        setName('')
+        setUsername('')
+        setEmail('')
+        setPassword('')
+        setHourlyRate('')
+        setAffiliation('')
+        setEducationalBackground('')
         setNewRequest(json)
-      console.log('new pharmacist registration request added:', json)
+        console.log('new pharmacist registration request added:', json)
+      }
+    }
+    catch (error){
+      alert(error.message)
     }
 
   }
 
   return (
-      <div>
-    <form className="create" onSubmit={handleSubmit}> 
-      <h2>Apply as a pharmacist to join the platform:</h2>
+      <div className="container mt-4">
+        <h1 className="mb-4">Apply as a pharmacist to join the platform:</h1>
+        <form className="create" onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label htmlFor="username" className="form-label">
+              Username:
+            </label>
+            <input
+                type="text"
+                className="form-control"
+                id="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="name" className="form-label">
+              Name:
+            </label>
+            <input
+                type="text"
+                className="form-control"
+                id="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">
+              E-mail:
+            </label>
+            <input
+                type="email"
+                className="form-control"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password" className="form-label">
+              Password:
+            </label>
+            <input
+                type="password"
+                className="form-control"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="dateOfBirth" className="form-label">
+              Date of Birth:
+            </label>
+            <input
+                type="date"
+                className="form-control"
+                id="dateOfBirth"
+                value={dateOfBirth}
+                onChange={(e) => setDateOfBirth(e.target.value)}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="hourlyRate" className="form-label">
+              Hourly Rate:
+            </label>
+            <input
+                type="number"
+                className="form-control"
+                id="hourlyRate"
+                value={hourlyRate}
+                onChange={(e) => setHourlyRate(e.target.value)}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="affiliation" className="form-label">
+              Affiliation:
+            </label>
+            <input
+                type="text"
+                className="form-control"
+                id="affiliation"
+                value={affiliation}
+                onChange={(e) => setAffiliation(e.target.value)}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="educationalBackground" className="form-label">
+              Educational Background:
+            </label>
+            <input
+                type="text"
+                className="form-control"
+                id="educationalBackground"
+                value={educationalBackground}
+                onChange={(e) => setEducationalBackground(e.target.value)}
+            />
+          </div>
 
-      <label>Username:</label>
-      <input 
-        type="text" 
-        onChange={(e) => setUsername(e.target.value)} 
-        value={username}
-      /> <br />
-
-      <label>Name:</label>
-      <input 
-        type="text" 
-        onChange={(e) => setName(e.target.value)} 
-        value={name}
-      /> <br />
-
-      <label>E-mail:</label>
-      <input 
-        type="email" 
-        onChange={(e) => setEmail(e.target.value)} 
-        value={email} 
-      /> <br />
-
-      <label>Password:</label>
-      <input 
-        type="password" 
-        onChange={(e) => setPassword(e.target.value)} 
-        value={password} 
-      /> <br />
-
-      <label>Date of Birth:</label>
-      <input 
-        type="date" 
-        onChange={(e) => setDateOfBirth(e.target.value)} 
-        value={dateOfBirth} 
-      /> <br />
-
-      <label>Hourly Rate:</label>
-      <input 
-        type="number" 
-        onChange={(e) => setHourlyRate(e.target.value)} 
-        value={hourlyRate} 
-      /> <br />
-
-      <label>Affiliation:</label>
-      <input 
-        type="text" 
-        onChange={(e) => setAffiliation(e.target.value)} 
-        value={affiliation} 
-      /> <br />
-
-      <label>Educational Background:</label>
-      <input 
-        type="text" 
-        onChange={(e) => setEducationalBackground(e.target.value)} 
-        value={educationalBackground} 
-      /> <br />
-
-
-      <button>Register</button>
-      {error && <div className="error">{error}</div>}
-    </form>
-          {newRequest && <PharmacistRequestDetails pharmacistRequest={newRequest} />}
+          <button className="btn btn-primary">Register</button>
+          {error && <div className="error">{error}</div>}
+        </form>
+        {newRequest && <PharmacistRequestDetails pharmacistRequest={newRequest} />}
       </div>
-  )
+  );
 }
 
 export default PharmacistRegistrationForm
