0b2f82dff83ab3eefd094a807c076b8cc44e7300
arwagadd <arwa_wail@hotmail.com>
2023-11-09 05:09:12 +0200

    medicine cart checkout

--- frontend/src/pages/MyCart.js
+++ frontend/src/pages/MyCart.js
@@ -5,6 +5,8 @@ import MedicineCartDetails from "../components/MedicineCartDetails";
 // View AvailableMedicines
 const MyCart = () => {
     const [medicines, setMedicines] = useState(null)
+    const [subtotalPrice, setSubtotalPrice] = useState(0);
+
     useEffect(() => {
         const fetchMedicines = async () => {
             try {
@@ -13,29 +15,68 @@ const MyCart = () => {
                 const json = await response.json()
                 if (response.ok) {
                     setMedicines(json)
-                }
-                else {
+                } else {
                     alert(await response.text())
                 }
-            }
-            catch (error){
+            } catch (error) {
                 alert(error.message)
             }
         }
         fetchMedicines()
     }, [])
 
+    useEffect(() => {
+        // Calculate subtotal price
+        const calculateSubtotalPrice = () => {
+            let subtotal = 0;
+            if (medicines) {
+                subtotal = medicines.reduce((total, medicine) => {
+                    return total + medicine.price * medicine.amount;
+                }, 0);
+            }
+            setSubtotalPrice(subtotal);
+        };
+
+        calculateSubtotalPrice();
+    }, [medicines]);
     return (
-        <div className="container">
-            <h2>My Cart</h2>
-            <div className="container available-medicines col-12 mt-5">
-                <div className="row">
-                    {medicines && medicines.map((medicine) => (
-                        <MedicineCartDetails key={medicine._id} medicine={medicine}/>
-                    ))}
+        <div className="container-order">
+            <div className="col-7">
+                <h2 className="left-part">My Cart</h2>
+                <div className="available-medicines mt-5 ">
+                    <div className="row row-cols-4 left-part">
+                        {medicines && medicines.map((medicine) => (
+                            <MedicineCartDetails key={medicine._id} className="col-3" medicine={medicine}/>
+                        ))}
+                    </div>
                 </div>
             </div>
+            <div className="order-summary col-5">
+                <h4 className="order-text">Order Summary</h4>
+                <p>
+                    <form className="coupon-form">
+                        <div className="form-group">
+                            <input type="text" className="form-control" placeholder="Enter coupon code" />
+                            <button type="submit" className="btn apply-btn">Apply</button>
+                        </div>
+                    </form>
+                </p>
+                <p className="sub-total">
+                    Subtotal: {subtotalPrice} EGP
+                </p>
+                <p className="sub-total">
+                    Shipping: 50 EGP
+                </p>
+                <hr/>
+                <h3 className="sub-total">
+                    Total: {subtotalPrice + 50} EGP
+                </h3>
+                <button type="button" className="btn btn-primary btn-lg checkout-btn">Checkout</button>
+
+            </div>
         </div>
+
+
     )
 }
 
