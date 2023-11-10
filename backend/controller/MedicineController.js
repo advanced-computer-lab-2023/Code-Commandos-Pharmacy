c1dd866c6ab710fa272cee6196486be85118223a
ebramfadl <ebram@ntsal.com>
2023-11-10 06:22:53 +0200

    i tried to fix arwa's bug but my head shut down after a very long day

--- backend/controller/MedicineController.js
+++ backend/controller/MedicineController.js
@@ -7,6 +7,7 @@ const multer = require('multer')
 const addOrUpdateMedicine = asyncHandler(async (req, res) => {
     // If the medicine is already in stock, update its quantity
     const medicineBody = req.body;
+    console.log(medicineBody)
     let med;
     try {
         med = await Medicine.findOne({ name: medicineBody.name });
@@ -56,7 +57,6 @@ const addOrUpdateMedicine = asyncHandler(async (req, res) => {
                 customerReviews,
                 customerRatings,
                 sales,
-                addedToCart,
                 amount,
                 imageUpload: file.path || null,
             });
